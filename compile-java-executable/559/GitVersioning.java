6a1589180331b5a03a1dba39e42c7a16e8eb2c52
Mohit Sabhadiya <mohit.sabhadiya@testgrid.io>
2025-12-23 00:04:29 +0000

    Test Case Updated

--- compile-java-executable/559/GitVersioning.java
+++ compile-java-executable/559/GitVersioning.java
@@ -1,12 +1,30 @@
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoClient;
-import org.bson.conversions.Bson;
-import org.bson.Document;
-import org.bson.BsonInt64;
-import org.bson.BsonDocument;
-import com.mongodb.*;
+import io.testgrid.listeners.TestListener;
+import io.testgrid.listeners.RetryFailedTestCases;
+import io.testgrid.tg;
+import org.testng.annotations.*;
+import app.getxray.xray.testng.annotations.XrayTest;
+import io.testgrid.enums.ComparisonType;
+import org.json.JSONObject;
+import io.testgrid.enums.Direction;
+import io.testgrid.enums.Size;
+import io.testgrid.enums.Buttons;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.*;
+import static io.testgrid.enums.KeyboardKeys.*;
+import org.openqa.selenium.support.ui.Select;
+import java.net.*;
+import java.util.*;
+import java.io.*;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 @Listeners(TestListener.class)
 public class GitVersioning {
+
+    @Test(retryAnalyzer = RetryFailedTestCases.class)
+    public void test1() {
+        tg.openBrowser();
+        tg.wait(2);
+        tg.close();
+    }
 }
