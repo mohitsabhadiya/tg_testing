78b5b7afbf8a59ebe91d91745b98249d609c18f8
Mohit Sabhadiya <mohit.sabhadiya@testgrid.io>
2025-12-24 03:20:44 +0530

    Single Maven Dependency Deleted

--- compile-java-executable/88/newOne.java
+++ compile-java-executable/88/newOne.java
@@ -1,23 +1,3 @@
-import io.testgrid.listeners.TestListener;
-import io.testgrid.listeners.RetryFailedTestCases;
-import io.testgrid.tg;
-import org.testng.annotations.*;
-import app.getxray.xray.testng.annotations.XrayTest;
-import io.testgrid.enums.ComparisonType;
-import org.json.JSONObject;
-import io.testgrid.enums.Direction;
-import io.testgrid.enums.Size;
-import io.testgrid.enums.Buttons;
-import static io.testgrid.baseClass.driver;
-import org.openqa.selenium.*;
-import static io.testgrid.enums.KeyboardKeys.*;
-import org.openqa.selenium.support.ui.Select;
-import java.net.*;
-import java.util.*;
-import java.io.*;
-import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
 @Listeners(TestListener.class)
 public class newOne {
 
@@ -26,18 +6,4 @@ public class newOne {
     public static String var_webString = "sttringgggggenergyyy";
 
     public static int var_webInt = 10;
-
-    public static void fcarddetails() {
-        tg.wait("ele_checkout719", ComparisonType.IS_VISIBLE);
-        tg.type("ele_email910", "John.tee@gmail.com");
-        tg.type("ele_phonenumbe689", "6734126754");
-        tg.type("ele_address565", "Storm breaker street 4126");
-        tg.type("ele_city300", "New York");
-        tg.type("ele_stateprovi773", "NY");
-        tg.type("ele_zippostalc150", "07008");
-        tg.type("ele_cardholder391", "John Tee");
-        tg.type("ele_cardnumber884", "5425233430109903");
-        tg.type("ele_cvv415", "252");
-        tg.selectDropdownByValue("ele_select872", "2026");
-    }
 }
