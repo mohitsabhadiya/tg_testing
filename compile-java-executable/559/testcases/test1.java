345f123e7bda459f83528f945aa63750bf175025
Mohit Sabhadiya <mohit.sabhadiya@testgrid.io>
2025-12-23 00:05:05 +0000

    Restore Version from Git Commit - Restored

--- compile-java-executable/559/testcases/test1.java
+++ compile-java-executable/559/testcases/test1.java
@@ -1,31 +1,16 @@
-import io.testgrid.listeners.TestListener;
-import io.testgrid.listeners.RetryFailedTestCases;
-import io.testgrid.tg;
-import org.testng.annotations.*;
-import app.getxray.xray.testng.annotations.XrayTest;
-import io.testgrid.enums.ComparisonType;
-import org.json.JSONObject;
-import io.testgrid.enums.Direction;
-import io.testgrid.enums.Size;
-import io.testgrid.enums.Buttons;
-import static io.testgrid.baseClass.driver;
-import org.openqa.selenium.*;
-import static io.testgrid.enums.KeyboardKeys.*;
-import org.openqa.selenium.support.ui.Select;
-import java.net.*;
-import java.util.*;
-import java.io.*;
-import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.Test;
-
-@Listeners(TestListener.class);
-public class test1 {
-
-	@Test(retryAnalyzer = RetryFailedTestCases.class)
-	public void test1() {
-		tg.openBrowser();
-		tg.wait(2);
-		tg.close();
-	}
-}
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import org.bson.conversions.Bson;
+import org.bson.Document;
+import org.bson.BsonInt64;
+import org.bson.BsonDocument;
+import com.mongodb.*;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import org.bson.conversions.Bson;
+import org.bson.Document;
+import org.bson.BsonInt64;
+import org.bson.BsonDocument;
+import com.mongodb.*;
