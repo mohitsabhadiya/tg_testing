20f486faf951c783e65dd7addcf2af0b3a4d4d6a
Mohit Sabhadiya <mohit.sabhadiya@testgrid.io>
2025-12-24 04:10:27 +0530

    Single Test Function Deleted

--- compile-java-executable/89/tet.java
+++ compile-java-executable/89/tet.java
@@ -77,4 +77,18 @@ public void a16dec0535() {
     tg.customScriptEnd();
     tg.close();
 }
+
+    public static void fcarddetails() {
+        tg.wait("ele_checkout719", ComparisonType.IS_VISIBLE);
+        tg.type("ele_email910", "John.tee@gmail.com");
+        tg.type("ele_phonenumbe689", "6734126754");
+        tg.type("ele_address565", "Storm breaker street 4126");
+        tg.type("ele_city300", "New York");
+        tg.type("ele_stateprovi773", "NY");
+        tg.type("ele_zippostalc150", "07008");
+        tg.type("ele_cardholder391", "John Tee");
+        tg.type("ele_cardnumber884", "5425233430109903");
+        tg.type("ele_cvv415", "252");
+        tg.selectDropdownByValue("ele_select872", "2026");
+    }
 }
